d0d3fe6982d1aeb87604372c5d7a33b3efd29a45
Abindu D <abindhar@tamu.edu>
2020-04-30 22:46:37 -0500

    Update productExceptSelf.java

--- src/productExceptSelf.java
+++ src/productExceptSelf.java
@@ -1,5 +1,7 @@
 public class productExceptSelf {
-    public int[] prodExceptSelf(int[] nums) {
+    public int[] solution(int[] nums) {
+        //return nonOptimal(nums);
+
         /* The output array does not count as extra space!!
         Algo: Run through input array left->right, then right->left
         Collecting whatever product needs to go in
@@ -19,6 +21,31 @@ public class productExceptSelf {
             prod *= nums[j];
             ans[j-1] *= prod;
         }
+        return ans;
+
+    }
+    public int[] nonOptimal(int[] nums) {
+        // Create two tmp arrays to hold left accumulate and right accumulated products
+        int n = nums.length;
+        if (n==1) return nums;
+        int[] left = new int[n];
+        int[] right = new int[n];
+        int[] ans = new int[n];
+        int prod = 1;
+        for (int i=0; i<n; i++){
+            prod *= nums[i];
+            left[i] = prod;
+        }
+        prod = 1;
+        for (int j=n-1; j>=0; j--){
+            prod *= nums[j];
+            right[j] = prod;
+        }
+        ans[0] = right[1];
+        for (int i=1; i<n-1; i++){
+            ans[i] = left[i-1] * right[i+1];
+        }
+        ans[n-1] = left[n-2];
         return ans;
     }
 }
