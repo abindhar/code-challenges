8e7f60bf16f06e0ae7cd7d21368267b7ccf2aa32
Abindu D <abindhar@tamu.edu>
2020-05-02 01:01:27 -0500

    Create threeSumSmaller.java

---
+++ src/threeSumSmaller.java
@@ -0,0 +1,26 @@
+public class threeSumSmaller {
+    // Leet 259
+    public int solution(int[] nums, int target) {
+        // Major hint is O(n^2) time, Sort + Two pointer should work
+        // [-2,-1,0,1,2,3] target=0
+        // [-2,0,1,3] target=4
+        int n = nums.length;
+        Arrays.sort(nums);
+        int ans = 0, sum = 0;
+        for (int i=0; i<n-2; i++){
+            int lo = i+1, hi = n-1;
+            while (lo<hi) {
+                sum = nums[i]+nums[lo]+nums[hi];
+                if (sum < target){
+                    // If nums[i]+nums[lo]+nums[hi]<target, then there are hi-lo
+                    // number of triplets
+                    ans += hi-lo;
+                    // Increase lo and recheck if nums[i]+nums[lo]+nums[hi]<target
+                    lo++;
+                } else hi--;
+            }
+        }
+        return ans;
+    }
+}
+}
